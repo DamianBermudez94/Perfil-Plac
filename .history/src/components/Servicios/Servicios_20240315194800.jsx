b0727392e62c100b566a36b5013fd80600816c7e
Damian Bermudez <bermudezdamian7@gmail.com>
2024-03-15 23:15:37 -0300

    Se comprimieFailed to parse source for import analysis because the content contains invalid JS syntax. You may need to install appropriate plugins to handle the .WebP file format, or if it's an asset, add **/*.WebP to  in your configuration.

---
+++ .history/src/components/Servicios/Servicios_20240315194800.jsx
@@ -0,0 +1,68 @@
+
+import { useEffect, useState } from "react";
+import debounce from 'lodash.debounce';
+import imagen1 from "../../../public/image/fondo1.WebP";
+import imagen2 from "../../../public/image/fondo2.WebP";
+import imagen3 from "../../../public/image/fondo3.WebP";
+import imagen4 from "../../../public/image/fondo4.WebP";
+import "./styles.css";
+
+const Servicios = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = debounce(() => {
+    const scrollY = window.scrollY;
+    const revealThreshold = 2800; 
+    
+    if (scrollY > revealThreshold) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  },200);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+  return (
+    <section className={`container-servicios scroll-reveal ${
+      isVisible ? "visible" : ""
+    }`} id="servicios">
+      <h1>Colocación</h1>
+      <div className="content-colocacion">
+      <h2 className="servicios-description">
+        Tus proyectos se vuelven realidad con nuestros servicios expertos en
+        colocación de durlock. ¡Confía en nosotros para transformar tus
+        espacios!
+      </h2>
+        <div className="container-img">
+          <figure>
+            <img src={imagen1} alt="Perfil-Plac" />
+          </figure>
+        </div>
+        <div className="container-img">
+          <figure>
+            <img src={imagen2} alt="Perfil-Plac" />
+          </figure>
+        </div>
+        <div className="container-img">
+          <figure>
+            <img src={imagen3} alt="Perfil-Plac" />
+          </figure>
+        </div>
+        <div className="container-img">
+          <figure>
+            <img src={imagen4} alt="Perfil-Plac" />
+          </figure>
+        </div>
+      </div>
+     
+    </section>
+  );
+};
+
+export default Servicios;
